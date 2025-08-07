4d066fca66603cd0266a48c7aa241c5ddf31b1cd
jaskasia <jjaskowiec@student.agh.edu.pl>
2025-08-07 19:31:46 +0200

    tests preparation

--- fpga/rtl/top_vga_basys3.sv
+++ fpga/rtl/top_vga_basys3.sv
@@ -12,89 +12,89 @@
  * Top level synthesizable module including the project top and all the FPGA-referred modules.
  */
 
- module top_vga_basys3 (
-    input  wire clk,
-    input  wire btnC,
-    output wire Vsync,
-    output wire Hsync,
-    output wire [3:0] vgaRed,
-    output wire [3:0] vgaGreen,
-    output wire [3:0] vgaBlue,
-    output wire JA1,
-    inout wire PS2Clk,
-    inout wire PS2Data
-);
+module top_vga_basys3 (
+        input  wire clk,
+        input  wire btnC,
+        output wire Vsync,
+        output wire Hsync,
+        output wire [3:0] vgaRed,
+        output wire [3:0] vgaGreen,
+        output wire [3:0] vgaBlue,
+        output wire JA1,
+        inout wire PS2Clk,
+        inout wire PS2Data
+    );
 
-timeunit 1ns;
-timeprecision 1ps;
+    timeunit 1ns;
+    timeprecision 1ps;
 
-/**
- * Local variables and signals
- */
+    /**
+     * Local variables and signals
+     */
 
-wire locked;
-wire pclk_mirror;
+    wire locked;
+    wire pclk_mirror;
 
-(* KEEP = "TRUE" *)
-(* ASYNC_REG = "TRUE" *)
-logic [7:0] safe_start = 0;
-logic clk40MHz;
-logic clk100MHz;
-// For details on synthesis attributes used above, see AMD Xilinx UG 901:
-// https://docs.xilinx.com/r/en-US/ug901-vivado-synthesis/Synthesis-Attributes
-
-
-/**
- * Signals assignments
- */
-
-assign JA1 = pclk_mirror;
+    (* KEEP = "TRUE" *)
+    (* ASYNC_REG = "TRUE" *)
+    logic [7:0] safe_start = 0;
+    logic clk40MHz;
+    logic clk100MHz;
+    // For details on synthesis attributes used above, see AMD Xilinx UG 901:
+    // https://docs.xilinx.com/r/en-US/ug901-vivado-synthesis/Synthesis-Attributes
 
 
-/**
- * FPGA submodules placement
- */
-clk_wiz_0_clk_wiz inst
-(
-// Clock out ports  
-.clk100MHz(clk100MHz),
-.clk40MHz(clk40MHz),
-// Status and control signals               
-.locked(locked),
-// Clock in ports
-.clk(clk)
-);
+    /**
+     * Signals assignments
+     */
+
+    assign JA1 = pclk_mirror;
 
 
-// Mirror pclk on a pin for use by the testbench;
-// not functionally required for this design to work.
-
-ODDR pclk_oddr (
-    .Q(pclk_mirror),
-    .C(clk40MHz),
-    .CE(1'b1),
-    .D1(1'b1),
-    .D2(1'b0),
-    .R(1'b0),
-    .S(1'b0)
-);
-
-
-/**
- *  Project functional top module
- */
-
-top_vga u_top_vga (
-    .clk40MHz(clk40MHz),
+    /**
+     * FPGA submodules placement
+     */
+    clk_wiz_0_clk_wiz inst
+    (
+    // Clock out ports  
     .clk100MHz(clk100MHz),
-    .rst(btnC),
-    .r(vgaRed),
-    .g(vgaGreen),
-    .b(vgaBlue),
-    .hs(Hsync),
-    .vs(Vsync),
-    .ps2_clk(PS2Clk),
-    .ps2_data(PS2Data)
-);
+    .clk40MHz(clk40MHz),
+    // Status and control signals               
+    .locked(locked),
+   // Clock in ports
+    .clk(clk)
+    );
+
+
+    // Mirror pclk on a pin for use by the testbench;
+    // not functionally required for this design to work.
+
+    ODDR pclk_oddr (
+        .Q(pclk_mirror),
+        .C(clk40MHz),
+        .CE(1'b1),
+        .D1(1'b1),
+        .D2(1'b0),
+        .R(1'b0),
+        .S(1'b0)
+    );
+
+
+    /**
+     *  Project functional top module
+     */
+
+    top_vga u_top_vga (
+        .clk40MHz(clk40MHz),
+        .clk100MHz(clk100MHz),
+        .rst(btnC),
+        .r(vgaRed),
+        .g(vgaGreen),
+        .b(vgaBlue),
+        .hs(Hsync),
+        .vs(Vsync),
+        .ps2_clk(PS2Clk),
+        .ps2_data(PS2Data)
+    );
 
 endmodule
